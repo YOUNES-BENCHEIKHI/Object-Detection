49d28c86645ad039e731faedeec643facf866cce
Younes <younesbencheikhi9@gmail.com>
2024-12-23 16:08:18 +0100

    Code.py

--- Code.py
+++ Code.py
@@ -98,19 +98,6 @@ if not cap.isOpened():
 model = YOLO("yolo-Weights/yolov8n.pt")
 print("Modèle YOLO chargé avec succès.")
 
-# Classes d'objets
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush","pin","stylo"
-              ]
-
 while True:
     success, img = cap.read()  # Lire chaque image de la vidéo
     if not success:
